0c51774d086dd1620cae9cd6b8f61b850fe0895e
Ali Tariq <alibabapk@aol.com>
2026-02-16 15:06:45 +0500

    variable value assignment

--- basics.py
+++ basics.py
@@ -17,3 +17,10 @@ print(8 / 5)  # division always returns a floating-point number
 print(5 ** 2)  # 5 squared
 
 print(2 ** 7)  # 2 to the power of 7
+
+width = 20
+height = 5 * 9
+print(width * height)
+# If a variable is not “defined” (assigned a value), trying to use it will give you an error:
+n
+print(n)
