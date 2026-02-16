25100906b55d296754e4ebbc5d47b5cbeff2ab4c
Ali Tariq <alibabapk@aol.com>
2026-02-16 23:29:33 +0500

    if statements

--- basics.py
+++ basics.py
@@ -207,3 +207,16 @@ a, b = 0, 1
 while a < 1000:
     print(a, end=', ')
     a, b = b, a+b
+
+
+x = int(input("Please enter an integer: "))
+
+if x < 0:
+    x = 0
+    print('Negative changed to zero')
+elif x == 0:
+    print('Zero')
+elif x == 1:
+    print('Single')
+else:
+    print('More')
