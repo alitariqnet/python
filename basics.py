1af4fceedb2323fca77002306311112366b2ded3
Ali Tariq <alibabapk@aol.com>
2026-02-16 15:23:16 +0500

    Text

--- basics.py
+++ basics.py
@@ -22,5 +22,29 @@ width = 20
 height = 5 * 9
 print(width * height)
 # If a variable is not “defined” (assigned a value), trying to use it will give you an error:
-n
-print(n)
+# n
+# print(n)
+
+tax = 12.5 / 100
+price = 100.50
+print(price * tax)
+
+# print(price + _)
+
+# print(round(_, 2))
+
+print('spam eggs')  # single quotes
+
+print("Paris rabbit got your back :)! Yay!")  # double quotes
+
+print('1975')  # digits and numerals enclosed in quotes are also strings
+
+print('doesn\'t')  # use \' to escape the single quote...
+
+print("doesn't")  # ...or use double quotes instead
+
+print('"Yes," they said.')
+
+print("\"Yes,\" they said.")
+
+print('"Isn\'t," they said.')
