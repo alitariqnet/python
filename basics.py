17b4b530b5c7ccd16e137264559c4803a155ede2
Ali Tariq <alibabapk@aol.com>
2026-02-16 17:32:23 +0500

    Lists

--- basics.py
+++ basics.py
@@ -122,3 +122,33 @@ word[:2] + 'py'
 
 s = 'supercalifragilisticexpialidocious'
 print(len(s))
+
+# LISTS
+
+squares = [1, 4, 9, 16, 25]
+print(squares)
+
+print(squares[0])  # indexing returns the item
+
+print(squares[-1])  
+
+print(squares[-3:])  # slicing returns a new list
+
+print(squares + [36, 49, 64, 81, 100])
+
+squares.append(99)
+
+print(squares)
+print(squares[2])
+print(squares[-1])
+
+cubes = [1, 8, 27, 65, 125]  # something's wrong here
+4 ** 3  # the cube of 4 is 64, not 65!
+
+cubes[3] = 64  # replace the wrong value
+print(cubes)
+
+cubes.append(216)  # add the cube of 6
+cubes.append(7 ** 3)  # and the cube of 7
+cubes
+print(cubes)
