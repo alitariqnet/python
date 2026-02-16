612f656fb0b27d8cd8a910e1080f26dcfcde5b1c
Ali Tariq <alibabapk@aol.com>
2026-02-16 15:02:12 +0500

    Mathematic operations

--- basics.py
+++ basics.py
@@ -1,1 +1,19 @@
 print("Hello, World!")
+# 
+print(17/3)
+print(17//3)
+print(17%3)
+print(++17)
+print(--17)
+
+print(2 + 2)
+
+print(50 - 5*6)
+
+print((50 - 5*6) / 4)
+
+print(8 / 5)  # division always returns a floating-point number
+
+print(5 ** 2)  # 5 squared
+
+print(2 ** 7)  # 2 to the power of 7
