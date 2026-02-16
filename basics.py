f825a5752476494d7f3b49fc16201eb130c090d6
Ali Tariq <alibabapk@aol.com>
2026-02-16 23:17:40 +0500

    Avoid newline with end= in while loop

--- basics.py
+++ basics.py
@@ -196,6 +196,14 @@ print(x[0][1])
 # Fibonacci series:
 # the sum of two elements defines the next
 a, b = 0, 1
-while a < 100:
+while a < 10:
     print(a)
+    a, b = b, a+b
+
+i = 256*256
+print('The value of i is', i)
+
+a, b = 0, 1
+while a < 1000:
+    print(a, end=', ')
     a, b = b, a+b
