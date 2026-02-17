594729aaaac612270b98a684249e0c3fc7172a30
Ali Tariq <alibabapk@aol.com>
2026-02-17 18:21:40 +0500

    else Clauses on Loops

--- basics.py
+++ basics.py
@@ -274,3 +274,23 @@ for num in range(2, 10):
         print(f"Found an even number {num}")
         continue
     print(f"Found an odd number {num}")
+
+
+for n in range(2, 100):
+    for x in range(2, n):
+        if n % x == 0:
+            # print(n, 'equals', x, '*', n//x)
+            break
+    else:
+        # loop fell through without finding a factor
+        print(n, 'is a prime number')
+
+
+someNum = 0
+
+while someNum < 10:
+    print(someNum)
+    someNum+=1
+else:
+    print(someNum)
+
