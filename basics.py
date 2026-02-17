b9c9c3d91a891b9db352bc8f226d1d68280aab6b
Ali Tariq <alibabapk@aol.com>
2026-02-17 23:07:11 +0500

    match keyword

--- basics.py
+++ basics.py
@@ -294,3 +294,45 @@ while someNum < 10:
 else:
     print(someNum)
 
+
+# while True:
+    # pass  # Busy-wait for keyboard interrupt (Ctrl+C)
+
+class MyEmptyClass:
+    pass
+
+# print("string".__qualname__)
+
+def http_error(status):
+    print("something")
+    match status:
+        case 400:
+            return "Bad request"
+        case 404:
+            return "Not found"
+        case 418:
+            return "I'm a teapot"
+        case _:
+            return "Something's wrong with the internet"
+
+
+print(http_error(100))
+
+# point is an (x, y) tuple
+def where_is(point):
+    match point:
+        case (0, 0):
+            print("Origin")
+        case (0, y):
+            print(f"Y={y}")
+        case (x, 0):
+            print(f"X={x}")
+        case (x, y):
+            print(f"X={x}, Y={y}")
+        case _:
+            raise ValueError("Not a point")
+        
+where_is((0, 0))
+where_is((0, 5))
+where_is((5, 0))
+where_is((5, 5))
