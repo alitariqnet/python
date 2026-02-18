c4308efecfceaef0ded5882c4ecff78149ca79ba
Ali Tariq <alibabapk@aol.com>
2026-02-18 22:42:29 +0500

    Function Annotations

--- basics.py
+++ basics.py
@@ -488,3 +488,11 @@ def my_function():
     pass
 
 print(my_function.__doc__)
+
+
+def f(ham: str, eggs: str = 'eggs') -> str:
+    print("Annotations:", f.__annotations__)
+    print("Arguments:", ham, eggs)
+    return ham + ' and ' + eggs
+
+f('spam')
