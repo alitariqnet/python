049784aed1b13895da302cf5da03047e16407159
Ali Tariq <alibabapk@aol.com>
2026-02-18 22:21:37 +0500

    Unpacking Argument Lists

--- basics.py
+++ basics.py
@@ -452,3 +452,11 @@ def concat(*args, sep="/"):
 print(concat("earth", "mars", "venus"))
 
 print(concat("earth", "mars", "venus", sep="."))
+
+def parrot(voltage, state='a stiff', action='voom'):
+    print("-- This parrot wouldn't", action, end=' ')
+    print("if you put", voltage, "volts through it.", end=' ')
+    print("E's", state, "!")
+
+d = {"voltage": "four million", "state": "bleedin' demised", "action": "VOOM"}
+parrot(**d)
