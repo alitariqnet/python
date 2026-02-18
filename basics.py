32cd1a11de440fed2d70c5e6681faf3eb3ca1a0f
Ali Tariq <alibabapk@aol.com>
2026-02-18 22:26:22 +0500

    lambda expressions

--- basics.py
+++ basics.py
@@ -460,3 +460,17 @@ def parrot(voltage, state='a stiff', action='voom'):
 
 d = {"voltage": "four million", "state": "bleedin' demised", "action": "VOOM"}
 parrot(**d)
+
+
+def make_incrementor(n):
+    return lambda x: x + n
+
+f = make_incrementor(42)
+print(f(0))
+
+print(f(1))
+
+
+pairs = [(1, 'one'), (2, 'two'), (3, 'three'), (4, 'four')]
+pairs.sort(key=lambda pair: pair[1])
+print(pairs)
