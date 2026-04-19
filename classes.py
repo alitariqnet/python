4be4b11a50617afecaf647c723d6cfc79fd02700
Ali Tariq <75094670+alitariqnet@users.noreply.github.com>
2026-04-19 15:13:18 +0500

    some classes

--- classes.py
+++ classes.py
@@ -1,3 +1,61 @@
-class users{
+class users:
 
-}
+    def scope_test():
+        def do_local():
+            spam = "local spam"
+
+        def do_nonlocal():
+            nonlocal spam
+            spam = "nonlocal spam"
+
+        def do_global():
+            global spam
+            spam = "global spam"
+
+        spam = "test spam"
+        do_local()
+        print("After local assignment:", spam)
+        do_nonlocal()
+        print("After nonlocal assignment:", spam)
+        do_global()
+        print("After global assignment:", spam)
+
+    scope_test()
+    print("In global scope:", spam)
+
+
+
+class MyClass:
+    """A simple example class"""
+    i = 12345
+
+    def f(self):
+        return 'hello world'
+    
+    def docstring(self):
+        return self.__doc__
+    
+mc = MyClass()
+print(mc.docstring)
+
+
+class Complex:
+    def __init__(self, realpart, imagpart):
+        self.r = realpart
+        self.i = imagpart
+
+x = Complex(3.0, -4.5)
+x.r, x.i
+
+
+
+class Warehouse:
+   purpose = 'storage'
+   region = 'west'
+
+w1 = Warehouse()
+print(w1.purpose, w1.region)
+
+w2 = Warehouse()
+w2.region = 'east'
+print(w2.purpose, w2.region)
